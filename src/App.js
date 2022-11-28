1a56da5b21a92f2b597d9c966e5ab0f13c6f99a5
1Mathias <mathiasabdisa@gmail.com>
2022-11-28 15:26:23 +0300

    Context updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { useContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { DarkModeContext } from "./context/darkModeContext";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 import New from "./pages/new/New";
 import Single from "./pages/single/Single";
+import './style/dark.scss'
 
 function App() {
+  const { darkMode } = useContext(DarkModeContext);
   return (
-    <div className="App">
+    <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/" >
